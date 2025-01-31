a421c443c6a2ddafc1d166b060090baa47aceca6
custodf <faline.custodio.da.silva@gmail.com>
2025-01-31 08:20:57 -0300

    Added pM.png and formatted pasteMods_v1.py.

--- pasteMods_v1.py
+++ pasteMods_v1.py
@@ -2,6 +2,7 @@ import re
 import time
 import pyperclip
 
+
 def clean_markdown(s):
     """
     Removes common Markdown syntax from the input string.
@@ -16,18 +17,22 @@ def clean_markdown(s):
         ("- "),
         ("> "),
         ("~~"),
-        ("---")
+        ("---"),
     ]
     for i in replacements:
         s = s.replace(i, "")
     return s.strip().lstrip()
 
+
 def clean_links(s):
     """
     Extracts and returns URLs from Markdown links and image syntax.
     """
-    cleaned = re.sub(r'!\[.*?\]\((https?://[^\)]+)\)|\[(.*?)\]\((https?://[^\)]+)\)', r'\1\3', s)
+    cleaned = re.sub(
+        r"!\[.*?\]\((https?://[^\)]+)\)|\[(.*?)\]\((https?://[^\)]+)\)", r"\1\3", s
+    )
     return cleaned
+
 
 def clean_lines(s):
     """
@@ -55,15 +60,17 @@ def process_clipboard():
                 cleaned_text = clean_lines(clean_markdown(clean_links(current_text)))
                 pyperclip.copy(cleaned_text)
         except pyperclip.PyperclipWindowsException:
-            exit('\nTimed out...') # Handles termination caused by Windows sleep mode.
+            exit("\nTimed out...")  # Handles termination caused by Windows sleep mode.
         time.sleep(0.5)
+
 
 def main():
 
     try:
         process_clipboard()
     except KeyboardInterrupt:
-        print('Program exited.')
+        print("Program exited.")
+
 
 if __name__ == "__main__":
     main()
